e3f93eb270fc3027319b40f1e820bf10e5bf5257
Valentin <valentin.gregor@codingmonkeys.net>
2022-08-10 15:12:16 +0200

    styling styling styling

--- client/src/components/PortfolioAboutMe/PortfolioAboutMe.js
+++ client/src/components/PortfolioAboutMe/PortfolioAboutMe.js
@@ -12,59 +12,65 @@ const openSite = (url) => {
 
 const PortfolioAboutMe = () => (
   <div className="PortfolioAboutMe">
-    <h2 className="intro-text">Hi my name is Jan</h2>
-    <div className="fuel-text">
-      <h3 className="welcome-text">WELCOME TO MY PERSONAL WEB PORTFOLIO</h3>
-      What fuels me is capturing interesting moments that happen around us.
-      Weather it is a one in a lifetime occasion or just common day to day stuff
-      every moment can be made great and is in a way, worth keeping. Feel free
-      to browse my personal web gallery.
+    <div className="left">
+      <h2 className="intro-text">Jan Rozman</h2>
     </div>
-    <div className="li-container">
-      <h3 className="equipment-header">EQUIPMENT:</h3>
-      <li>Camera - Canon eos r5</li>
-      <li>Strobes - Quadralite ad600 pro, Quadralite stroboss 60 basic</li>
-      <li>
-        Lenses - Canon ef 16-35mm f/2.8l usm ii, Sigma art 50mm f/1.4, igma
-        70-200mm f/2.8
-      </li>
-      <li>Gimbal - DJI RSC 2 gimbal</li>
-      <li>Tripod - 2x Godox 260T 260cm Air, Manfrotto MK290</li>
-      <li>Softbox - Quadralite hexadecagon 90cm, Aputure ligh dome mini II</li>
-    </div>
-    <div className="sm-cont">
-      <div className="contact-container">
-        <div className="contact-header">CONTACT:</div>
-        <div className="contact-text">
-          <li>
-            <a className="" href="tel:+38641519414">
-              Tel: +38641519414
-            </a>
-          </li>
-          <li>Mail: Rozman.jan00@gmail.com</li>
+    <div className="right">
+      <div className="fuel-text">
+        <h3 className="welcome-text">WELCOME TO MY PERSONAL WEB PORTFOLIO</h3>
+        What fuels me is capturing interesting moments that happen around us.
+        Weather it is a one in a lifetime occasion or just common day to day
+        stuff every moment can be made great and is in a way, worth keeping.
+        Feel free to browse my personal web gallery.
+      </div>
+      <div className="li-container">
+        <h3 className="equipment-header">EQUIPMENT:</h3>
+        <span>Camera - Canon eos r5</span>
+        <span>Strobes - Quadralite ad600 pro, Quadralite stroboss 60 basic</span>
+        <span>
+          Lenses - Canon ef 16-35mm f/2.8l usm ii, Sigma art 50mm f/1.4, igma
+          70-200mm f/2.8
+        </span>
+        <span>Gimbal - DJI RSC 2 gimbal</span>
+        <span>Tripod - 2x Godox 260T 260cm Air, Manfrotto MK290</span>
+        <span>
+          Softbox - Quadralite hexadecagon 90cm, Aputure ligh dome mini II
+        </span>
+      </div>
+      <div className="sm-cont">
+        <div className="contact-container">
+          <div className="contact-header">CONTACT:</div>
+          <div className="contact-text">
+            <span>
+              <a className="" href="tel:+38641519414">
+                Tel: +38641519414
+              </a>
+            </span>
+            <span>Mail: Rozman.jan00@gmail.com</span>
+          </div>
+        </div>
+        <div class="logo-sm-cont">
+          <img
+            className="logo-sm"
+            src={igLogo}
+            onClick={() => openSite("https://www.instagram.com/clarityst/")}
+          />
+          <img
+            className="logo-sm"
+            src={fbLogo}
+            onClick={() =>
+              openSite("https://www.facebook.com/Clarityst-111557937087687/")
+            }
+          />
+          <img
+            className="logo-sm"
+            src={gmailLogo}
+            onClick={() => openSite("https://mailto:Rozman.jan00@gmail.com")}
+          />
         </div>
       </div>
-      <div class="logo-sm-cont">
-        <img
-          className="logo-sm"
-          src={igLogo}
-          onClick={() => openSite("https://www.instagram.com/clarityst/")}
-        />
-        <img
-          className="logo-sm"
-          src={fbLogo}
-          onClick={() =>
-            openSite("https://www.facebook.com/Clarityst-111557937087687/")
-          }
-        />
-        <img
-          className="logo-sm"
-          src={gmailLogo}
-          onClick={() => openSite("https://mailto:Rozman.jan00@gmail.com")}
-        />
-      </div>
+      <div className="mobile-divider"></div>
     </div>
-    <div className="mobile-divider"></div>
   </div>
 );
 
